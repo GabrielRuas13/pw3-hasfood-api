a22299f758a966383d0bb1b6ece4523734df08bf
Gabriel Ruas <gabrielruas2004@gmail.com>
2022-03-28 08:58:54 -0300

    Criacao da entidade Avaliacao

--- src/main/java/br/com/gabriel/ruas/pw3hasfoodapi/entity/Avaliacao.java
+++ src/main/java/br/com/gabriel/ruas/pw3hasfoodapi/entity/Avaliacao.java
@@ -1,17 +1,11 @@
 package br.com.gabriel.ruas.pw3hasfoodapi.entity;
 
-import java.time.LocalDate;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
-import br.com.gabriel.ruas.pw3hasfoodapi.enums.*;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -19,20 +13,20 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(name = "TBL_PEDIDO")
+@Table(name = "TBL_AVALIACAO")
 
-public class Pedido {
+public class Avaliacao {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "ID_PEDIDO")
+	@Column(name = "ID_AVALIACAO")
 	private Long id;
+	 
+	@Column(name = "TX_COMENTARIO")
+	private String nome;
 	
-	@Column(name = "DT_PEDIDO")
-	private LocalDate data;
+	@Column(name = "TX_NOTA")
+	private Integer nota;
 	
-	@Column(name = "TX_STATUS")
-	private String status;
-	
-	@Column(name = "ID_RESTAURANTE")
-	private Integer restaurante;
+	@Column(name = "ID_PEDIDO")
+	private String pedido;
 }
